ddb895c978c9c18819f079a3986c3b01f2bb05b6
timoparv65 <timo_p65@yahoo.com>
2015-10-28 11:39:42 +0200

    Päivän 28.10.2015 työt

--- controllers/scripts.js
+++ controllers/scripts.js
@@ -1,3 +1,4 @@
+"use strict"; // jslint antaa varoituksen kaikista koodin osista, jotka ei hyvin
 console.log("Here we go!!"); // menee selaiseen logiin. Crome=>lisää tyäkaluja/kehittäjän työkalut. Tulee consoleen debuggerissa
 
 // selain luo window-ohjektin, luo myäs navigator (selainhistoria) ja document-objektit
@@ -46,10 +47,34 @@ $(document).ready(function(){
     //$("[class]").css("background-color","lightblue") //
     $("#head").css("background-color","lightblue").css("padding","20px").css("border-radius","8px");
     
-    $(".about").text("<b>New text</b>") // tässä näkyy <b> ja </b>
+    $(".about").text("<b>New text</b>"); // tässä näkyy <b> ja </b>
     //$(".about").html("<b>New text</b>") // t
     
-    $("[data-dummy]").html("<p>Hello World</p>");
+    $("[data-dummy]").html("<p>Hello World</p>"); // hae elementti data-dummy
+    
+    var settings = { // luodaan objekti ajax:ia varten
+        method:"GET", // löytyy jquery API => AJAX
+        url:"http://localhost:28017/oma/person/",
+        dataType:"jsonp",
+        jsonp:"jsonp" // voidaan rikkoa cross domain policya
+    };
+    
+    $.ajax(settings).done(function(data){// lähettään pyynnön url osoitteeseen. Jää odottamaan responsea, joka otetaan kiinni done-funktiolla
+        console.log(data);
+        
+        for(var i = 0; i < data.rows.length; i++){ // käydään taulukko läpi. rows => kts. selaimen debuggerista
+            var html = "<tr>" + // luodaan table row
+                        "<td>" + data.rows[i].name + "</td>" + // luodaan table data
+                        "<td>" + data.rows[i].address + "</td>" +
+                        "<td>" + data.rows[i].age + "</td>" +
+                        "</tr>";
+            
+            $(html).appendTo("tbody") // lisätään html-dokumentin <tdody> elementtiin
+        }
+        
+    });
+    
+    //$.ajax({}); // {} on anonyymi objekti. Objektin voi määrittää myös erikseen
     
 });
 
